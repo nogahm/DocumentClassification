e5dd31553c9cbdcd3581cb2f9527e005f907b874
nogahm <nogahm@post.bgu.ac.il>
2018-12-18 12:59:32 +0200

    nogah

--- temp.py
+++ temp.py
@@ -1,13 +1,18 @@
 import os
 import nltk
+import sklearn
+import pandas as pd
+import numpy as np
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from os import listdir
 from os.path import isfile, join
+import sklearn.datasets as sk
 
 #variables
-dirpath=os.getcwd()+'\\ohsumed-first-20000-docs'
+path='d:\\documents\\users\\nogahm\\Downloads\\testCorpus'
+dirpath=path
 trainDirs=[]
 testDirs=[]
 trainFiles=[]
@@ -17,13 +22,29 @@ Category=[]
 FileName=[]
 Path=[]
 Text=[]
+
+classNumOfDocs=[]
+docsInfo=[] #[fileName,cleanText,class
+
 stopWords=set(stopwords.words('english'))
 
 # get train and test dirs
 trainDirs=os.listdir(dirpath+'\\training')
-print(dirpath)
 testDirs=os.listdir(dirpath+'\\test')
-# extract category, name, path and text
+
+# pre-process
+def cleanText(text):
+    # tokenize
+    tokenized = word_tokenize(text)
+    # remove stop words
+    filtered_sentence = [w for w in tokenized if not w in stopWords]
+    # stemming
+    ps = PorterStemmer()
+    for i in range(len(filtered_sentence)):
+        filtered_sentence[i] = ps.stem(filtered_sentence[i])
+    return " ".join(filtered_sentence)
+
+# get train and test dirs
 for dir in trainDirs:
     for file in os.listdir(dirpath+'\\training\\'+dir):
         trainFiles.append(file)
@@ -33,34 +54,44 @@ for dir in trainDirs:
         # Open a file: fileReader
         fileReader = open(dirpath+'\\training\\'+dir+'\\'+file,mode='r')
         # read all lines at once
-        text = fileReader.read()
+        text = fileReader.read().lower()
         # close the file
         fileReader.close()
-        Text.append(text)
+
+        cleanedText=cleanText(text)
+        docsInfo.append([file,cleanedText,dir])
+
 for dir in testDirs:
-    for file in os.listdir(dirpath+'\\training\\'+dir):
+    for file in os.listdir(dirpath+'\\test\\'+dir):
         testFiles.append(file)
         Category.append(dir)
         FileName.append(file)
-        Path.append(dirpath+'\\training\\'+dir+'\\'+file)
+        Path.append(dirpath+'\\test\\'+dir+'\\'+file)
         # Open a file: fileReader
-        fileReader = open(dirpath+'\\training\\'+dir+'\\'+file,mode='r')
+        fileReader = open(dirpath+'\\test\\'+dir+'\\'+file,mode='r')
         # read all lines at once
-        text = fileReader.read()
+        text = fileReader.read().lower()
         # close the file
         fileReader.close()
-        Text.append(text.lower()) #get to lower case: pre-processing
 
+        cleanedText = cleanText(text)
+        docsInfo.append([file, cleanedText, dir])
 
-# pre-process
-for index in range(len(Text)):
-    # tokenize
-    tokenized=word_tokenize(Text[index])
-    # remove stop words
-    filtered_sentence = [w for w in tokenized if not w in stopWords]
-    # stemming
-    ps = PorterStemmer()
-    for i in range(len(filtered_sentence)):
-        filtered_sentence[i]=ps.stem(filtered_sentence[i])
-    Text[index]=filtered_sentence
+#get number of classes and number of docs for each class
+docInfoDF=pd.DataFrame(docsInfo, columns=['fileName','cleanText','class'])
+allClasses=np.unique(docInfoDF['class'])
+print ('# of categories: ',allClasses.size)
+numOfFiles=[]
+termsFreqPerClass=[]
+for currClass in allClasses:
+    temp=(docInfoDF['class']).value_counts()[currClass]
+    numOfFiles.append([currClass, temp])
+    #get terms distibution
+    currFiles=docInfoDF.loc[docInfoDF['class']==currClass]
+    termsFreq=pd.Series(" ".join(currFiles['cleanText']).split()).value_counts()[:10]
+    termsFreqPerClass.append([currClass] + termsFreq.index)
+
+numOfFiles=pd.DataFrame(numOfFiles,columns=['class','#of docs'])
+print(numOfFiles)
+print(termsFreqPerClass)
 
